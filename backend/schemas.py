4e27e032cf733361b522ed23a322c5149c9eb7d9
Ahmed Elawady <87013310+AhmedElawady74@users.noreply.github.com>
2024-09-20 21:20:15 +0500

    ..

---
+++ backend/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+    role: str
+
+class User(BaseModel):
+    id: int
+    email: str
+    role: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True
